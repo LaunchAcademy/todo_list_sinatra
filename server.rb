ffdca68657a6aa4632f58c4fa78682647f76c724
Adam Sheehan <a.t.sheehan@gmail.com>
2014-02-12 11:02:24 -0500

    Render task list from a template.

--- server.rb
+++ server.rb
@@ -1,3 +1,11 @@
 require 'sinatra'
 
+get '/' do
+  @tasks = ['pay bills', 'buy milk', 'learn Ruby']
+  erb :index
+end
+
+# These lines can be removed since they are using the default values. They've
+# been included to explicitly show the configuration options.
+set :views, File.dirname(__FILE__) + '/views'
 set :public_folder, File.dirname(__FILE__) + '/public'
